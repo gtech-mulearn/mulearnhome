8d13c3a61316d52adf086643d7425a9ad0be0ba0
Amith Abey Stephen <145148320+Amith-Abey-Stephen@users.noreply.github.com>
2025-10-02 16:49:10 +0530

    Remove remotePatterns from next.config.ts
    Removed remote image patterns from Next.js config.

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.example.com', // replace with your CDN host
-        pathname: '/**',             // allow all paths
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',        // allow UFO image host
-        pathname: '/**',
-      },
+    
     ],
   },
 };
