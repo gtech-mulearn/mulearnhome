2805626992e48f7ca0ff459712331e883441cece
ziyaudheenms <msharafudeen193@gmail.com>
2025-10-08 05:41:27 +0530

    converted the card css to tailwind

--- src/app/community-partners/CommunityCard/CommunityCard.tsx
+++ src/app/community-partners/CommunityCard/CommunityCard.tsx
@@ -16,28 +16,28 @@ const CommunityCard: React.FC<CommunityCardProps> = ({
   customlink,
 }) => {
   return (
-    <div className={styles.community_card_container}>
-      <div className={styles.community_card}>
+    <div className="my-4 max-w-xs shadow-[1px_1px_45px_-5px_rgba(0,0,0,0.08)] p-4 rounded-xl h-24 flex items-center justify-evenly">
+      <div>
         {clink && clink.length > 0 && (
           <a href={clink} target="_blank" rel="noopener noreferrer">
-            <div className={styles.box}>
-              <Image src={cimage} alt={cname} className={styles.communityimg}  width={200}
+            <div className="flex justify-between items-center">
+              <Image src={cimage} alt={cname} className="w-16 rounded-md object-contain mr-8"  width={200}
   height={100}/>
 
               {cname && (
-                <span className={styles.community_interest}>{cname}</span>
+                <span className="text-gray-500 text-[0.9rem] text-right font-semibold w-[150px]">{cname}</span>
               )}
             </div>
           </a>
         )}
         {customlink && customlink.length > 0 && (
           <Link href={customlink}>
-            <div className={styles.box}>
-               <Image src={cimage} alt={cname} className={styles.communityimg}  width={200}
+            <div className="flex justify-between items-center">
+               <Image src={cimage} alt={cname} className="w-16 rounded-md object-contain mr-8"  width={200}
   height={100}/>
 
               {cname && (
-                <span className={styles.community_interest}>{cname}</span>
+                <span className="text-gray-500 text-sm text-right font-semibold w-[150px]">{cname}</span>
               )}
             </div>
           </Link>
